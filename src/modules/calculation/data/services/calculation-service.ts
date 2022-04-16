5ae74c5f71877cfaa00590565332a844676f6605
ducthach1401 <ducthach.1403@gmail.com>
2022-04-17 00:25:42 +0700

    update

--- src/modules/calculation/data/services/calculation-service.ts
+++ src/modules/calculation/data/services/calculation-service.ts
@@ -5,7 +5,12 @@ import { lastValueFrom } from 'rxjs';
 @Injectable()
 export class CalculationService {
   constructor(private readonly httpService: HttpService) {}
+  private readonly PE = 7;
+  private readonly Risk = 3.6;
+  private readonly Y = 4.7;
+
   async calculateBenjamin(): Promise<Record<string, any>> {
+    this.getNormalizedEps('FLC');
     const listStock = await this.getNameStockHose();
     return listStock;
   }
@@ -21,5 +26,32 @@ export class CalculationService {
       }
     }
     return result;
+  }
+
+  async getEpsStock(name: string) {
+    const dataStock = await lastValueFrom(
+      this.httpService.get(`https://e.cafef.vn/fi.ashx?symbol=${name}`),
+    );
+    let result = [];
+    for (const data of dataStock.data) {
+      result.push(data.EPS);
+    }
+    return result.reverse();
+  }
+
+  async recipeBenjamin(nEps: number, g: number) {
+    return (nEps * (this.PE + g) * this.Risk) / this.Y;
+  }
+
+  async getNormalizedEps(nameStock: string) {
+    const stockEps = await this.getEpsStock(nameStock);
+    let abnormalEps = [];
+    let normalEps = [];
+    let temp = 0;
+    for (let eps of stockEps) {
+      temp += eps;
+    }
+    temp = temp / stockEps.length;
+    console.log(temp);
   }
 }
