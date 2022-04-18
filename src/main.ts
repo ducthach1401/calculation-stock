5cd0522b3c81b27deaf8771075ad13dff235d17d
ducthach1401 <ducthach.1403@gmail.com>
2022-04-18 17:08:17 +0700

    update version

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { AppModule } from './app/app-module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
-    defaultVersion: '1',
     type: VersioningType.URI,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
--- src/modules/calculation/app/http/controllers/calculation-controller.ts
+++ src/modules/calculation/app/http/controllers/calculation-controller.ts
@@ -3,7 +3,10 @@ import { Response } from 'express';
 import { CalculateBenjaminUsecase } from 'src/modules/calculation/domain/usecases/calculate-benjamin-usecase';
 import { GetBenjaminUsecase } from 'src/modules/calculation/domain/usecases/get-benjamin-usecase';
 
-@Controller('stock')
+@Controller({
+  version: '1',
+  path: 'stock',
+})
 export class CalculationController {
   constructor(
     private readonly calculateBenjaminUsecase: CalculateBenjaminUsecase,
